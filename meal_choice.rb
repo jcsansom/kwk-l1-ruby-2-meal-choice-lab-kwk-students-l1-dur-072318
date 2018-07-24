4285e400b4e17a7c8377bd0810c369899e5d2d0a
jcsansom <jayceesansom@gmail.com>
2018-07-24 14:43:10 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,11 +2,13 @@ def snacks(food="Cheetos")
   "Any time, is the right time for #{food}!"
 end
 
-
+snacks()
 
 def breakfast(default_meal_choice_breakfest="frosted flakes")
   puts "Morning is the best time for #{default_meal_choice_breakfest}!"
 end
+
+
 
 def lunch(default_meal_choice_lunch="grilled cheese")
   puts "The afternoon is the best time for #{default_meal_choice_lunch}!"
