a7026ec53d5c6986a2145d2dbea7b1af60b30d1b
jcsansom <jayceesansom@gmail.com>
2018-07-24 14:39:19 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,3 +1,17 @@
+def breakfast (default_meal_choice_breakfest="frosted flakes")
+  puts "Morning is the best time for #{default_meal_choice_breakfest}!"
+end
 
+def lunch (default_meal_choice_lunch="grilled cheese")
+  puts "The afternoon is the best time for #{default_meal_choice_lunch}!"
+end
+
+def dinner (default_meal_choice_dinner="salmon")
+  puts "The evening is the best time for #{default_meal_choice_dinner}!"
+end
+
+breakfast("pancakes")
+lunch("peanut butter")
+dinner("broccoli")
 
 
